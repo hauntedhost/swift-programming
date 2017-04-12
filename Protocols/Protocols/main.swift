00a8dabc7ba458f39b96a5b45dab3149f5ef177e
Sean Omlor <somlor@eml.cc>
2017-04-12 00:07:10 -0400

    got carried away with text formatting

--- Protocols/Protocols/main.swift
+++ Protocols/Protocols/main.swift
@@ -8,20 +8,78 @@
 
 import Foundation
 
-func printTable(_ data: [[String]]) {
-  for row in data {
-    var out = "|"
-    for item in row {
-      out += " \(item) |"
-    }
-    print(out)
+extension Array {
+  subscript (safe index: Index) -> Element? {
+    return index < count ? self[index] : nil
   }
 }
 
-let data = [
-  ["Sean", "373", "422"],
-  ["Castle", "42", "72"],
-  ["Alli", "923", "989"],
+func leftPad(_ string: String, _ len: Int) -> String {
+  let paddingCount = max(0, len - string.characters.count)
+  let padding = repeatElement(" ", count: paddingCount).joined(separator: "")
+  return "\(string)\(padding)"
+}
+
+func maxColumnWidths(_ rows: [[String]]) -> [Int] {
+  var columnWidths: [Int] = []
+  for row in rows {
+    for (i, item) in row.enumerated() {
+      let itemWidth = item.characters.count
+      if let currentWidth = columnWidths[safe: i] {
+        columnWidths[i] = itemWidth > currentWidth ? itemWidth : currentWidth
+      } else {
+        columnWidths.append(itemWidth)
+      }
+    }
+  }
+  return columnWidths
+}
+
+func dividerFor(columnWidths: [Int]) -> String {
+  return "+" + columnWidths.map {
+    repeatElement("-", count: $0 + 2).joined(separator: "")
+  }.joined(separator: "+") + "+"
+}
+
+func printTable(_ rows: [[String]], columnLabels: String...) {
+
+  let columnWidths: [Int] = maxColumnWidths(rows + [columnLabels])
+  let divider = dividerFor(columnWidths: columnWidths)
+
+  print(divider)
+
+  var labelRow = "|"
+  for (i, columnLabel) in columnLabels.enumerated() {
+    labelRow += " \(leftPad(columnLabel, columnWidths[i])) |"
+  }
+  print(labelRow)
+
+  print(divider)
+
+  for row in rows {
+    var dataRow = "|"
+    for (i, item) in row.enumerated() {
+      dataRow += " \(leftPad(item, columnWidths[i])) |"
+    }
+    print(dataRow)
+  }
+
+  print(divider)
+}
+
+let rows = [
+  ["Alli", "26", "Meridian"],
+  ["Castle", "1", "Rutherfordton"],
+  ["Sean", "41", "Philadelphia"],
+  ["Sebastian", "13", "Oakland"],
 ]
 
-printTable(data)
+let rowsWithState = [
+  ["Alli", "26", "Meridian", "MS"],
+  ["Castle", "1", "Rutherfordton", "NC"],
+  ["Sean", "41", "Philadelphia", "PA"],
+  ["Sebastian", "13", "Oakland", "CA"],
+]
+
+printTable(rows, columnLabels: "Name", "Age", "City")
+printTable(rowsWithState, columnLabels: "Name", "Age", "City", "State")
